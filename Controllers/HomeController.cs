01e293ce868b3b90ae7d2ec2fd2a66db3bcd8ed6
fleeNasser <alexander.agiasotis@gmail.com>
2025-10-18 20:02:10 +1100

    Improved Navigation Bar
    Added Stores button and MyReviews button (IF the user is logged in).

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,6 +33,10 @@ namespace MatchaReviewApp.Controllers
         {
             return View();
         }
+        public IActionResult Stores()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
